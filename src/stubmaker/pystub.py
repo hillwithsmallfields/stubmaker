9dcce9113203c3fa3da69a1c84dd452da97592a2
John Sturdy <john.sturdy@planarific.com>
2024-06-07 15:07:37 +0100

    improve arg handling and introduce separation of concerns

--- src/stubmaker/pystub.py
+++ src/stubmaker/pystub.py
@@ -10,10 +10,29 @@ def get_args():
     parser.add_argument("--fileinput", "-f", action='store_true')
     parser.add_argument("--json", "-j", action='store_true')
     parser.add_argument("--yaml", "-y", action='store_true')
-    parser.add_argument("output")
+    parser.add_argument("--output", "-o")
     return vars(parser.parse_args())
 
+def arg_name(arg):
+    """Return the name part of an arg."""
+    return arg.split('.')[0]
+
+def arg_type(arg):
+    """Return the type part of an arg."""
+    return arg.split('.')[1].removesuffix('+').removesuffix('*').removesuffix(':').removesuffix('%') if '.' in arg else None
+
+TYPE_READERS = {
+    'csv': "[f(x) for x in csv.DictReader(%s_stream)]",
+    'json': "json.load(%s_stream)",
+    'yaml': "yaml.safeload(%s_stream)",
+    }
+
 def pystub(args, csv, fileinput, json, yaml, output):
+    input_args = []
+    arg_types = {arg_name(arg): arg_type(arg) for arg in args}
+    csv |= 'csv' in arg_types.values()
+    json |= 'json' in arg_types.values()
+    yaml |= 'yaml' in arg_types.values()
     with open(output, 'w') as outstream:
         progname = os.path.splitext(os.path.basename(output))[0]
         outstream.write("#!/usr/bin/env python3\n\n")
@@ -23,8 +42,6 @@ def pystub(args, csv, fileinput, json, yaml, output):
             outstream.write("import csv\n")
         if fileinput:
             outstream.write("import fileinput")
-            args.append("inputspec+")
-        else:
             args.append("inputspec+")
         if json:
             outstream.write("import json\n")
@@ -41,41 +58,53 @@ def pystub(args, csv, fileinput, json, yaml, output):
                 if arg.endswith("+"):
                     action = ", action='append'"
                     arg = arg.removesuffix("+")
-                if arg.endswith("*"):
+                elif arg.endswith("*"):
                     action = ", nargs='*'"
                     arg = arg.removesuffix("*")
-                if arg.endswith(":"):
+                elif arg.endswith(":"):
                     action = ", action='store_true'"
                     arg = arg.removesuffix(":")
+                elif arg.endswith("%"):
+                    arg = arg.removesuffix("%")
+                    input_args.append(arg)
                 args[iarg] = arg
                 outstream.write("""    parser.add_argument("%s%s"%s%s)\n""" %
                                 ("" if iarg == len(args) - 1 else "--",
                                  arg, extra, action))
-            outstream.write("""    return vars(parser.parse_args())\n\n""")
-        outstream.write("""def %s(%s):\n""" % (progname,
+            outstream.write("""    return vars(parser.parse_args())\n\n\n""")
+        outstream.write("""def %s(%s):\n    return foo\n\n\n"""
+                        % (progname,
+                           ", ".join(arg_types.keys())))
+        outstream.write("""def %s_main(%s):\n""" % (progname,
                                             ", ".join(args)))
-        if args and (csv or json or yaml):
+        if args and (csv or json or yaml or 'output' in args):
             has_config = 'config' in args and yaml
             if has_config:
                 outstream.write("""    with open(config) as confstream:\n""")
                 outstream.write("""        config = yaml.safeload(confstream)\n""")
             if fileinput:
                 outstream.write("""    with fileinput.input(files=inputspec) as instream:\n""")
+            elif input_args:
+                outstream.write("""    with """
+                                + ", ".join("""open(%s) as %s_stream""" % (arg_name(arg), arg_name(arg))
+                                            for arg in input_args)
+                                + ":\n")
             else:
-                outstream.write("""    with open(inputspec) as instream:\n""")
-            if csv:
-                outstream.write("""        data = [f(x) for x in csv.DictReader(instream)]\n""")
-            elif json:
-                outstream.write("""        data = json.load(instream)\n""")
-            elif yaml and not has_config:
-                outstream.write("""        data = yaml.safeload(instream)\n""")
+                outstream.write("""        data = instream.read()\n""")
+            outstream.write("""        result = %s(\n""" % progname)
+            for argname, argtype in arg_types.items():
+                if argname != "output":
+                    outstream.write("""            %s=%s,\n""" % (argname,
+                                                                  TYPE_READERS.get(argtype, "%s_stream.read()") % argname))
+            outstream.write("""        )\n""")
             if 'output' in args:
-                outstream.write("""    with open(output) as outstream:\n""")
-                outstream.write("""        outstream.write(data)\n""")
+                outstream.write("""    with open(output, 'w') as outstream:\n""")
+                outstream.write("""        result.save(outstream)\n""")
+            outstream.write("""    return result\n""")
         else:
             outstream.write("""    pass\n\n""")
         outstream.write("""\nif __name__ == "__main__":\n""")
-        outstream.write("""    %s(**args)\n""" % progname)
+        outstream.write("""    %s_main(**get_args())\n""" % progname)
 
 if __name__ == "__main__":
     pystub(**get_args())
